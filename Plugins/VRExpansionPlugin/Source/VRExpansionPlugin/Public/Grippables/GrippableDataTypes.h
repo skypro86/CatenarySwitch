f8596a310ba7f05653573a93e5c0430ef0dd6858
Rares Orza <rares.orza@gmail.com>
2026-02-18 17:25:35 +0100

    Initial commit

---
+++ Plugins/VRExpansionPlugin/Source/VRExpansionPlugin/Public/Grippables/GrippableDataTypes.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "CoreMinimal.h"
+//#include "Engine/EngineTypes.h"
+#include "Engine/ReplicatedState.h"
+#include "GrippableDataTypes.generated.h"
+
+// A version of the attachment structure that include welding data
+USTRUCT()
+struct VREXPANSIONPLUGIN_API FRepAttachmentWithWeld : public FRepAttachment
+{
+public:
+	GENERATED_BODY()
+
+	// Add in the is welded property
+	UPROPERTY()
+	bool bIsWelded;
+
+	// Not really needed, removing it makes sure Iris will just work
+	//bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+	FRepAttachmentWithWeld();
+};
+
+/*template<>
+struct TStructOpsTypeTraits< FRepAttachmentWithWeld > : public TStructOpsTypeTraitsBase2<FRepAttachmentWithWeld>
+{
+	enum
+	{
+		WithNetSerializer = true//,
+		//WithNetSharedSerialization = true,
+	};
+};
+*/
